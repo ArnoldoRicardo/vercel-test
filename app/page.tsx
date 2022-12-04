4ef3f254a690182e658d3fc6e63d3540e266ce88
ArnoldoRicardo <anyback@gmail.com>
2022-12-04 16:34:41 -0600

    fix index

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,18 @@ export default async function Page() {
   // const user = await getCurrentUser()
   return (
     <>
-      users:
+      user:
       <ul>
         {/* {users && users.map((user) => <li key={user.id}>{user.username}</li>)} */}
       </ul>
+      list of some apps
       <ul>
         <li>
-          experimental
-          <Link href="/book/0">primer book</Link>
-        </li>
-        <li>
-          legacy
-          <Link href="/todos/0">primer book</Link>
+          <Link href="/todos/0"> Todo app</Link>
         </li>
       </ul>
+      experimental
+      <Link href="/book/0">primer book</Link>
     </>
   )
 }
